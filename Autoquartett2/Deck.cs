99e8220b102cc4dbc78186103660a353f9201af2
Sarah Schallehn <sarah.s17@schule.koeln>
2020-11-24 12:14:37 +0100

    02-Deck init push
    Erstellen von Klassen Deck + Player

---
+++ Autoquartett2/Deck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoquartett2
+{
+    class Deck
+    {
+        /**
+         * Liste, die 32 Objekte des Typs Car enthält 
+         */
+        private LinkedList<Car> cars = new LinkedList<Car>();
+
+        public Deck()
+        {
+            InitateCards();
+        }
+
+        /*
+         *Liest einzelnen Karten aus einer JSON Datei aus und speichert sie als Objekte des Typs Car 
+         */
+        private bool InitateCards()
+        {
+            return true;
+        }
+
+        /*
+         *Mischt die Karten des Decks 
+        */
+        public void ShuffleCars()
+        {
+
+        }
+        /*
+         * Teilt alle Karten gleichmäßig auf die teilnehmenden Spieler auf. Lässt sie Anzahl an Karten nicht glatt durch die Spieleranzahl teilen,
+         * werden wird der nächst kleinere Teiler verwendet und die übrigen Karten ignoriert.
+         */
+        public bool DistributeCars(List<Player> playerList)
+        {
+            int toMuch = cars.Count % playerList.Count();
+
+            for (int i = 0; i < (playerList.Count() - toMuch); i++)
+            {
+                int playerNumber = i % playerList.Count();
+                playerList.ElementAt(playerNumber).AddCar(cars.ElementAt(i));
+            }
+
+            return true;
+        }
+    }
+}
