02f1b774218eb7bdfa9436323bfe4e6be311c8f6
Leon Schirra <leon.s33@schule.koeln>
2020-11-26 19:55:27 +0100

    Compare

--- Autoquartett2/BusinessLogic.cs
+++ Autoquartett2/BusinessLogic.cs
@@ -49,6 +49,92 @@ namespace Autoquartett2
         public string GetPlayerInput()
         {
             return Console.ReadLine();
+        }
+
+        public double GetInput(List<Car> carList)
+        {
+            bool higherNumber = true;
+            string playerInput;
+            int stringValue;
+
+            playerInput = Console.ReadLine();
+
+            switch (playerInput)
+            {
+                case "1":
+                case "beschleunigung":
+                    higherNumber = false;
+                    stringValue = 1;
+                    break;
+
+                case "2":
+                case "hubraum":
+                    stringValue = 2;
+                    break;
+
+                case "3":
+                case "verbrauch":
+                    higherNumber = false;
+                    stringValue = 3;
+                    break;
+
+                case "4":
+                case "geschwindigkeit":
+                    stringValue = 4;
+                    break;
+
+                case "5":
+                case "leistung":
+                    stringValue = 5;
+                    break;
+
+                case "6":
+                case "zylinder":
+                    stringValue = 6;
+                    break;
+
+                default:
+                    stringValue = 0;
+                    break;
+            }
+            return FillArray(stringValue, higherNumber, carList);
+            
+
+        }
+        public double FillArray(int value, bool higherNumber, List<Car> carList) 
+        {
+            double[] values = new double[4];
+            for (int j = 0; j < carList.Count; j++) {
+                switch (value)
+                {
+                    case 1:
+                        values[j] = carList[j].GetAcceleration();
+                        break;
+
+                    case 2:
+                        values[j] = carList[j].GetPiston();
+                        break;
+
+                    case 3:
+                        values[j] = carList[j].GetConsumption();
+                        break;
+
+                    case 4:
+                        values[j] = carList[j].GetKmPerH();
+                        break;
+
+                    case 5:
+                        values[j] = carList[j].GetPs();
+                        break;
+
+                    case 6:
+                        values[j] = carList[j].GetPiston();
+                        break;
+                }
+            }
+            double erg = Car.Comparison(values, higherNumber);
+
+            return erg;
         }              
     }
 }
